e80deba969872acc71a764421d21560dd0012ccc
Chip Long <chip_long@yahoo.com>
2021-05-12 18:43:38 -0600

    licenses work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const makeMD = require("./utils/generateMarkdown");
@@ -8,18 +9,41 @@ const makeMD = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    message: "Please enter the project name: ",
-    name: "project",
+    message: "Please enter the project name (required): ",
+    name: "title",
   },
   {
     type: "input",
-    message: "Please enter the project name: ",
-    name: "project",
+    message:
+      "Provide a short description explaining the what, why, and how of your project (required): ",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "Which license would you like to use (optional): ",
+    choices: [
+      "",
+      "MIT",
+      "GNU GPLv3",
+      "The Unlicense",
+      new inquirer.Separator(),
+      "GNU AGPLv3",
+      "GNU LGPLv3",
+      "Mozilla Public License 2.0",
+      "Apache License 2.0",
+      "Boost Software License 1.0",
+    ],
+    name: "license",
+    loop: false,
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // path-join method
+  console.log(fileName, ":");
+  console.log(data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,45 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Return a license badge based on which license is passed in (If there is no license, return an empty string)
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Returns the license link
 function renderLicenseLink(license) {
-  return `*[License](#license)`;
+  return license ? `*[License](#license)` : "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Return the license section of README (If there is no license, return an empty string)
+function renderLicenseSection(license) {
+  let licenseLoc;
+  let licenseLocations = [
+    ["MIT", "mit"],
+    ["GNU GPLv3", "gpl-3.0"],
+    ["The Unlicense", "unlicense"],
+    ["GNU AGPLv3", "agpl-3.0"],
+    ["GNU LGPLv3", "lgpl-3.0"],
+    ["Mozilla Public License 2.0", "mpl-2.0"],
+    ["Apache License 2.0", "apache-2.0"],
+    ["Boost Software License 1.0", "bsl-1.0"],
+  ];
 
-// TODO: Create a function to generate markdown for README
+  if (!license || license === "") {
+    return "";
+  }
+
+  for (let elem of licenseLocations) {
+    if (elem[0] === license) {
+      licenseLoc = elem[1];
+      break;
+    }
+  }
+
+  return `
+
+## License
+
+This project is licensed under the terms of the ${license} license. For more information please, refer to [https://choosealicense.com/](https://choosealicense.com/${licenseLoc}/).
+`;
+}
+
+// Generate the markdown for README (each render function displays its own lines)
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge}
